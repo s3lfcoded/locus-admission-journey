e01d920631ed0c4e580ff379e5593495eacbb7b0
s3lfcoded <s3lfcoded@users.noreply.github.com>
2026-09-17 16:17:27 +0300

    .

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   server: {
     host: "0.0.0.0",
-    allowedHosts: ["terminal.local"],
+    allowedHosts: true,
     warmup: {
       clientFiles: ["./src/main.jsx"],
     },
